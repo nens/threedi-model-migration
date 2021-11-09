e940b2f71c1a77da83a593e73779f39fda6ffa2c
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-11-09 12:39:30 +0100

    Add pytest dep

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -43,7 +42,11 @@ setup(
     packages=find_packages(include=['threedi_model_migration', 'threedi_model_migration.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/caspervdw/threedi_model_migration',
+    extras_require={
+        "test": test_requirements,
+    },
+    python_requires=">=3.7",
+    url='https://github.com/nens/threedi_model_migration',
     version='0.1.0',
     zip_safe=False,
 )
