65ed90dbd84da9917f9d6272daf8a8e9bae4dcb5
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-11-22 13:33:15 +0100

    Add sqlite compression

--- threedi_model_migration/schematisation.py
+++ threedi_model_migration/schematisation.py
@@ -10,6 +10,9 @@ from typing import Optional
 __all__ = ["Schematisation", "SchemaRevision"]
 
 
+SQLITE_COMPRESSION_RATIO = 7
+
+
 @dataclass
 class SchemaRevision:
     sqlite_path: Path  # relative to repository dir
@@ -37,7 +40,7 @@ class Schematisation:
     settings_id: int
     settings_name: str  # the newest of its revisions
     file_count: Optional[int] = None
-    total_size: Optional[int] = None
+    total_size_mb: Optional[int] = None
     revisions: Optional[List[SchemaRevision]] = None
 
     @property
@@ -45,14 +48,16 @@ class Schematisation:
         return f"{self.slug}-{self.sqlite_name}-{self.settings_name}"
 
     def summarize_files(self):
-        unique_files = set()
+        unique_sqlites = set()
+        unique_rasters = set()
         for revision in self.revisions:
-            unique_files.add(revision.sqlite)
-            unique_files |= set(revision.rasters)
-        unique_files = list(unique_files)
+            unique_sqlites.add(revision.sqlite)
+            unique_rasters |= set(revision.rasters)
 
-        self.file_count = len(unique_files)
-        self.total_size = sum(x.size for x in unique_files)
+        self.file_count = len(unique_sqlites) + len(unique_rasters)
+        size_sqlites = sum(x.size for x in unique_sqlites) / SQLITE_COMPRESSION_RATIO
+        size_rasters = sum(x.size for x in unique_rasters)
+        self.total_size_mb = int((size_sqlites + size_rasters) / (1024 ** 2))
 
     def __repr__(self):
         return f"Schematisation({self.slug}, sqlite_name={self.sqlite_name}, settings_name={self.settings_name})"
