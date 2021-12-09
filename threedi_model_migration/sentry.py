c89dabb0230ad429ae9c3c3bdfd6becf7e1e68b4
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-12-09 16:23:17 +0100

    Hide warning logs in sentry

--- threedi_model_migration/sentry.py
+++ threedi_model_migration/sentry.py
@@ -7,6 +7,6 @@ import sentry_sdk
 def setup_sentry(dsn):
     sentry_logging = LoggingIntegration(
         level=logging.INFO,  # Capture info and above as breadcrumbs
-        event_level=logging.WARNING,  # Send errors and warnings as events
+        event_level=logging.ERROR,  # Send errors and warnings as events
     )
     sentry_sdk.init(dsn=dsn, integrations=[sentry_logging])
