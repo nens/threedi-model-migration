33d0c2d02cbfcab8693b055908ec91ea556fb398
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2022-01-03 18:52:31 +0100

    Finish

--- threedi_model_migration/schematisation.py
+++ threedi_model_migration/schematisation.py
@@ -36,7 +36,7 @@ class SchemaRevision:
 @dataclass
 class Schematisation:
     repo_slug: str
-    sqlite_name: str  # the newest of its revisions
+    sqlite_path: Path  # the newest of its revisions
     settings_id: int
     settings_name: str  # the newest of its revisions
     metadata: Optional[SchemaMeta] = None
@@ -44,10 +44,41 @@ class Schematisation:
 
     @property
     def name(self):
-        return f"{self.repo_slug} - {self.sqlite_name}_{self.settings_name}"[:256]
+        """Construct the name of the schematisation.
+
+        Due to legacy considerations, the start of the name is:
+
+          {repo_slug} - {sqlite filename}_{global settings name}
+
+        However this leads to non-unique slugs in many cases. Because of that we add
+        some extra info between parentheses:
+
+          {settings_id}[, directory: {sqlite directory}]
+
+        Resulting names are for example:
+
+        - v2_bergermeer - v2_bergermeer_simple_infil_no_grndwtr (1)
+        - schermerboezem - schermer_1d2d_default (1)
+        - v2_bergermeer - bergermeer_Bergermeer (1, directory: model)
+        """
+        # The 'mdu_name' is formatted exactly like inpy does for threedimodels
+        mdu_name = f"{self.sqlite_path.stem}_{self.settings_name}"
+        # To make the name unique, include the containing dir and settings id as
+        # extra info between parentheses
+        if str(self.sqlite_path.parent) != ".":
+            extra_info = f", directory: {self.sqlite_path.parent}"
+        else:
+            extra_info = ""
+        return (f"{self.repo_slug} - {mdu_name} ({self.settings_id}{extra_info})")[:256]
 
     @property
     def slug(self):
+        """Slugified version of name, examples:
+
+        - v2_bergermeer-v2_bergermeer_simple_infil_no_grndwtr-1
+        - schermerboezem-schermer_1d2d_default-1
+        - v2_bergermeer-bergermeer_bergermeer-1-directory-model
+        """
         return slugify(self.name)
 
     def get_files(self) -> Set[File]:
